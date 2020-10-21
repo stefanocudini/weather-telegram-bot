0595c697b31abf8caa2c175628d62d7aec1fbd72
stefanocudini <stefano.cudini@gmail.com>
2020-10-21 21:23:11 +0200

    fix nord

--- util.js
+++ util.js
@@ -2,6 +2,6 @@
 module.exports = {
 	azimut: function(ang) {
 		var texts = "n,nne,ne,ene,e,ese,se,sse,s,ssw,sw,wsw,w,wnw,nw,nnw,n";
-		return ang ? texts.split(',')[Math.round(ang/22.5)] : '';
+		return texts.split(',')[Math.round(parseFloat(ang)/22.5)];
 	}
 };
